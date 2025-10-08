e6cb75410fdfe6be9865cbbc055f800cad906e22
unknown <bigdaddy2030@gmail.com>
2025-10-08 13:34:57 +0100

    added backedn

---
+++ lib/axiosClient.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosClient;
